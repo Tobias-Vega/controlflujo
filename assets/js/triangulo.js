55b2596d94cc721fca549251e35ea96ede3094db
Tobias <tobiasvega1210@gmail.com>
2024-05-13 11:42:24 -0300

    se realizó el segundo ejercicio

---
+++ assets/js/triangulo.js
@@ -0,0 +1,14 @@
+let i = 0
+let numeral = "#"
+
+while (i < 5) {
+    if (i == 0) {
+        console.log("#")
+
+    } else if (i > 0) {
+        numeral = numeral + "#"
+        console.log(numeral)
+    }
+
+    i = i + 1
+}
